ce65eb603e5045a8995e4b758cf27401ffd19f77
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-12-18 14:35:58 +0300

    Update schema.rb with new table for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_17_191404) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_17_203026) do
   create_table "bands", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_17_191404) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["band_id"], name: "index_members_on_band_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "members", "bands"
